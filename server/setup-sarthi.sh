a1224801f3c1ce856d0e98d07037ddced7ec2bcb
tushar5526 <codingid6@gmail.como>
2023-12-28 21:25:46 +0000

    feat: add support to export logs

--- server/setup-sarthi.sh
+++ server/setup-sarthi.sh
@@ -9,7 +9,7 @@ NC='\033[0m' # No Color
 # Emojis
 CHECK_MARK='✔️'
 CROSS_MARK='❌'
-WARNING='⚠️'
+POINT='👉'
 DOCKER='🐳'
 
 # Check if script is run as root
@@ -18,31 +18,62 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-# Update package index
-echo -e "${YELLOW}${WARNING} Updating package index...${NC}"
-apt update
+# # Update package index
+# echo -e "${YELLOW}${POINT} Updating package index...${NC}"
+# apt update
 
-# Install prerequisites
-echo -e "${YELLOW}${WARNING} Installing prerequisites...${NC}"
-apt install -y apt-transport-https ca-certificates curl software-properties-common
+# # Install prerequisites
+# echo -e "${YELLOW}${POINT} Installing prerequisites...${NC}"
+# apt install -y apt-transport-https ca-certificates curl software-properties-common
 
-# Install Docker
-echo -e "${YELLOW}${WARNING} Installing Docker...${NC}"
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-rm get-docker.sh
+# # Install Docker
+# echo -e "${YELLOW}${POINT} Installing Docker...${NC}"
+# curl -fsSL https://get.docker.com -o get-docker.sh
+# sh get-docker.sh
+# rm get-docker.sh
 
-# Add user to docker group
-echo -e "${YELLOW}${WARNING} Adding user to the docker group...${NC}"
-usermod -aG docker $USER
-newgrp docker
+# # Add user to docker group
+# echo -e "${YELLOW}${POINT} Adding user to the docker group...${NC}"
+# usermod -aG docker $USER
+# newgrp docker
 
+# # Install Docker Compose
+# echo -e "${YELLOW}${POINT} Installing Docker Compose...${NC}"
+# curl -sSL https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+# chmod +x /usr/local/bin/docker-compose
 
-# Install Docker Compose
-echo -e "${YELLOW}${WARNING} Installing Docker Compose...${NC}"
-curl -sSL https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+# Start Grafaa + Loki services
+echo -e "${YELLOW}${POINT} Starting Loki + Grafana to export logs ${NC}"
+docker-compose up -d promtail loki grafana
+
+# Install Loki Docker Driver
+echo -e "${YELLOW}${POINT} Installing Loki Docker Driver...${NC}"
+docker plugin install grafana/loki-docker-driver:2.9.1 --alias loki --grant-all-permissions
+
+# Configure Docker Daemon for Loki Logging
+echo -e "${YELLOW}${POINT} Configuring Docker Daemon for Loki Logging...${NC}"
+cat <<EOF > /etc/docker/daemon.json
+{
+    "debug" : true,
+    "log-driver": "loki",
+    "log-opts": {
+        "loki-url": "http://localhost:3100/loki/api/v1/push",
+        "loki-batch-size": "400",
+        "loki-retries": "1",
+        "loki-max-backoff": "500ms",
+        "loki-timeout": "1s",
+        "keep-file": "true"
+    }
+}
+EOF
+
+# Restart Docker Daemon
+echo -e "${YELLOW}${POINT} Restarting Docker Daemon...${NC}"
+systemctl restart docker
+
+echo -e "${YELLOW}${POINT} Recreate All Docker Services to register logger changes${NC}"
+docker-compose up -d --force-recreate
 
 # Display success message
-echo -e "${GREEN}${CHECK_MARK} Docker and Docker Compose installed successfully.${NC}"
+echo -e "${GREEN}${CHECK_MARK} Docker, Docker Compose, and Loki Docker Driver installed and configured successfully.${NC}"
 echo -e "${DOCKER} ${YELLOW}You may need to restart your shell or log out and log back in to apply the changes.${NC}"
